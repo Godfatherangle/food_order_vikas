e8c48fab7a4e733cccc4f1a817b1b819965957bc
Godfatherangle <ssverma33a@gmail.com>
2023-11-22 09:43:58 -0800

    first commit

---
+++ src/app/services/food.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { sample_foods, sample_tags } from 'src/data';
+// import { FOODS_BY_SEARCH_URL, FOODS_BY_TAG_URL, FOODS_TAGS_URL, FOODS_URL, FOOD_BY_ID_URL } from '../shared/constants/urls';
+import { Food } from '../shared/models/Food';
+import { Tag } from '../shared/models/Tag';
+
+@Injectable({ 
+  providedIn: 'root'
+})
+export class FoodService {
+  
+  constructor(){}
+  getAll(): Food[] {
+    return sample_foods
+  }
+
+
+getAllFoodsBySearchTerm(searchTerm: string) {
+  return this.getAll().filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()))
+}
+  
+getAllTags():Tag[]{
+
+  return sample_tags
+}
+
+getAllFoodsByTag(tag:string): Food[]{
+  return tag === "All" ?
+  this.getAll():
+  this.getAll().filter(food => food.tags?.includes(tag))
+}
+getFoodById(foodId:string):Food{
+  return this.getAll().find(food => food.id === foodId) ?? new Food()
+}
+}
